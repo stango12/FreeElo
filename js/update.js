e92f2fd74257bd00b23c310189782d7d5ba0089d
stango12 <stevenyztang@gmail.com>
2016-07-21 19:31:09 +0800

    Initial commit

---
+++ js/update.js
@@ -0,0 +1,81 @@
+var main = function() {
+	var freeWeekChamps = httpGet("https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=491b6446-c673-4e8a-a877-2568ad964a6a");
+	var ids = [];
+    var keys = [];
+
+    var n = 0;
+
+    for(i = 0; i < freeWeekChamps.length; i++)
+    {
+    	        	var temp = "";
+    	while(freeWeekChamps.charAt(i) >= "0" && freeWeekChamps.charAt(i) <= "9")
+    	{
+    		temp += freeWeekChamps.charAt(i);
+    		i++;
+    	}
+    	if(temp !== "" && temp !== "0")
+    	{
+
+        	ids[n] = temp;
+        	n++;
+    	}
+    }
+
+    var test = "";
+	for(i = 0; i < ids.length; i++)
+	{
+		if(ids[i] != 0)
+		{
+            var link = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+ ids[i] + "?api_key=491b6446-c673-4e8a-a877-2568ad964a6a";
+		    ids[i] = IDtoChamp(httpGet(link));
+            keys[i] = IDtoKey(httpGet(link));
+            test += ids[i] + " key: " + keys[i] + ", ";
+        }
+	}
+
+    		    $('.first-point').text(test);
+    document.getElementById("champ0").href = "champions/azir.html";
+};
+
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
+    return xmlhttp.responseText;
+}
+
+function IDtoChamp(id)
+{
+    var index = id.indexOf("\"name\"") + 8;
+    var champName = "";
+    while(id.charAt(index) !== '\"')
+    {
+        champName += id.charAt(index);
+        index++;
+    }
+
+    return champName;
+}
+
+function IDtoKey(id)
+{
+    var index = id.indexOf("\"key\"") + 7;
+    var champName = "";
+    while(id.charAt(index) !== '\"')
+    {
+        champName += id.charAt(index);
+        index++;
+    }
+
+    return champName;
+}
+
+$(document).ready(main);
