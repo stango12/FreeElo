222eecb1e77ae4cb7f78573b86a15c378810b79e
Steven <stevenyztang@gmail.com>
2017-07-23 15:34:31 -0400

    securing api

--- js/update.js
+++ js/update.js
@@ -1,44 +1,33 @@
 var main = function() {
-	var freeWeekChamps = httpGet("https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=APIKEY");
-	var ids = [];
+    var ids = httpGet("http://127.0.0.1:5000/freeChamps").split(" ");
     var keys = [];
 
     var n = 0;
-
-    for(i = 0; i < freeWeekChamps.length; i++)
+    //gets the names and keys for the champs that are free
+    var test = "";
+    for(i = 0; i < ids.length; i++)
     {
-    	        	var temp = "";
-    	while(freeWeekChamps.charAt(i) >= "0" && freeWeekChamps.charAt(i) <= "9")
-    	{
-    		temp += freeWeekChamps.charAt(i);
-    		i++;
-    	}
-    	if(temp !== "" && temp !== "0")
-    	{
-
-        	ids[n] = temp;
-        	n++;
-    	}
+        if(ids[i] != 0)
+        {
+            var link = "http://127.0.0.1:5000/champion/" + ids[i];
+            ids[i] = IDtoChamp(httpGet(link));
+            keys[i] = IDtoKey(httpGet(link));
+        }
     }
 
-    var test = "";
-	for(i = 0; i < ids.length; i++)
-	{
-		if(ids[i] != 0)
-		{
-            var link = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+ ids[i] + "?api_key=APIKEY";
-		    ids[i] = IDtoChamp(httpGet(link));
-            keys[i] = IDtoKey(httpGet(link));
-            test += ids[i] + " key: " + keys[i] + ", ";
-        }
-	}
-
-    		    $('.first-point').text(test);
-    document.getElementById("champ0").href = "champions/azir.html";
+    //updating the carousel to have the right champs
+    for(i = 0; i < ids.length; i++)
+    {
+        document.getElementById("champ" + i).href = "champions/" + keys[i] + ".html";
+        document.getElementById("pic" + i).src = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + keys[i] + "_0.jpg";
+        document.getElementById("name" + i).innerHTML = ids[i];
+    }
+    
 };
 
 function httpGet(theUrl)
 {
+    //taken from SO
     if (window.XMLHttpRequest)
     {// code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
@@ -52,6 +41,7 @@ function httpGet(theUrl)
     return xmlhttp.responseText;
 }
 
+//takes in ID, gets the name of the champion
 function IDtoChamp(id)
 {
     var index = id.indexOf("\"name\"") + 8;
@@ -65,6 +55,7 @@ function IDtoChamp(id)
     return champName;
 }
 
+//takes in ID, gets the key of the champion
 function IDtoKey(id)
 {
     var index = id.indexOf("\"key\"") + 7;
